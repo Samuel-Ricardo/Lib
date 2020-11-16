c8c3c25d934065bdbce3df29e91420b3b350e7c4
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2020-11-16 00:29:19 -0300

    Created: Function - public boolean isBissexto(int year) (Numbers/Year)

--- src/Numbers/Year.java
+++ src/Numbers/Year.java
@@ -10,5 +10,15 @@ package Numbers;
  * @author Samuel
  */
 public class Year {
+ 
+    public boolean isBissexto(int year){
     
+        if((year%4 == 0 && year%400 == 0) || year%100 != 0){
+            
+            return true;
+        }else{
+        
+            return false;
+        }
+    }
 }
