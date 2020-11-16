ad104610a40b6b4e71c30cae5594c5cb974c8c69
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2020-11-16 00:47:47 -0300

    Test: vogal.IsVogal() // (String/Test) //

--- src/String/Test.java
+++ src/String/Test.java
@@ -11,4 +11,18 @@ package String;
  */
 public abstract class Test {
     
+    public static void main(String[] args) {
+        
+        Vogal vogal = new Vogal();
+        
+        System.out.println("è vogal? "+vogal.isVogal("a"));
+        System.out.println("è vogal? "+vogal.isVogal("e"));
+        System.out.println("è vogal? "+vogal.isVogal("i"));
+        System.out.println("è vogal? "+vogal.isVogal("o"));
+        System.out.println("è vogal? "+vogal.isVogal("u"));
+        System.out.println("è vogal? "+vogal.isVogal("b"));
+        System.out.println("è vogal? "+vogal.isVogal("f"));
+        System.out.println("è vogal? "+vogal.isVogal("g"));
+    }
+    
 }
